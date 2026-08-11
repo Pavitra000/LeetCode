a0c7936fdfa9e537f83672fd83fe3a7ed3d46bf7
Pavitra000 <146116518+Pavitra000@users.noreply.github.com>
2026-08-12 02:41:39 +0530

    Time: 3 ms (52.20%) | Memory: 14 MB (94.10%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    void solve(vector<int> &candidates, int target, int i, int sumi, vector<int> &tempAns, vector<vector<int>> &ans){
+        if(i>=candidates.size() || sumi > target){sumi = 0; return;}
+        if(sumi == target){ans.push_back(tempAns); sumi = 0; return;}
+
+        sumi = sumi+candidates[i];
+        tempAns.push_back(candidates[i]);
+        solve(candidates, target, i, sumi, tempAns, ans);
+        sumi = sumi-candidates[i];
+        tempAns.pop_back();
+        solve(candidates, target, i+1, sumi, tempAns, ans);
+        return;
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> tempAns;
+        solve(candidates, target, 0, 0, tempAns, ans);
+
+        return ans;
+    }
+};
