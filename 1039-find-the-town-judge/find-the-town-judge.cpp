c86993154c6e72e40ee885e3bbcecd8920fc1553
Pavitra000 <146116518+Pavitra000@users.noreply.github.com>
2026-08-29 15:41:14 +0530

    Time: 44 ms (10.73%) | Memory: 73.3 MB (24.03%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int findJudge(int n, vector<vector<int>>& trust) {
-        vector<int> trustsSomeone(n + 1, 0);
-        vector<int> trustedBySomeone(n + 1, 0);
+        vector<int> score(n + 1, 0);
 
-        if(n==1){return 1;}
-        for(auto it: trust){
-            trustsSomeone[it[0]]++;
-            trustedBySomeone[it[1]]++;
+        for (auto it : trust) {
+            score[it[0]]--;
+            score[it[1]]++;
         }
 
-        for(int i=0; i<=n; i++){
-            if(trustsSomeone[i]==0 && trustedBySomeone[i]==n-1){return i;}
+        for (int i = 1; i <= n; i++) {
+            if (score[i] == n - 1)
+                return i;
         }
 
         return -1;
